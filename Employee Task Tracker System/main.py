e572110109489867bd5adec6946a4d8167ac366c
Laiba Ashar <asharmunir642@gmail.com>
2026-05-20 16:30:37 +0500

    Initial Commit

--- Employee Task Tracker System/main.py
+++ Employee Task Tracker System/main.py
@@ -2,53 +2,50 @@ Employees={}
 def add_employee(employee_name):
     if employee_name not in Employees:
         Employees[employee_name]=[]
-        print(f"{employee_name}added successfully")
-        
+        print(f"Employee '{employee_name}'added successfully")
     else:
-        print("Employee Already Exists")
+        print("Employee already exists")
 def assign_task(employee_name,task_name):
-    if task_name  in Employees:
+    if employee_name in Employees:
         task={
-            'task_name':'Login_System',
-            'status':'Pending',
+            "task_name":task_name,
+            "Status":"Pending"
         }
-    Employees[employee_name].append('task')
-    print(f"Task'{task_name}' assigned to'{employee_name}")
+        Employees[employee_name].append(task)
+        print(f"Task '{task_name}' assigned to {employee_name}")
     else:
-    print("Employee Not Found")
-def complete_task(employee_name,task_name):
+        print("Employee not found")
+def complete_task (employee_name,task_name):
     if employee_name in Employees:
         for task in Employees[employee_name]:
-          if task["task_name"]==task_name:
-               task["status"]="Completed"
-               print(f"Task'{task_name}'completed")
-               return
-          print("task Not Found")
+            if task["task_name"]==task_name:
+                task["Status"]="Completed"
+                print(f"Task '{task_name}'completed")
+                return 
+            print("Task not found")
         else:
-            print("Employee Not Found")
+            print("Employee not found")
 def get_employee_tasks(employee_name):
     if employee_name in Employees:
         return Employees[employee_name]
-    return "Employee Not Found"
+    return "Employee not found"
 def get_pending_tasks():
     pending_tasks=[]
-    for employee_name,tasks in Employees.items():
-        pending_lists=[]
-        for task in tasks:
-            if task["status"]=="Pending":
-                pending_lists.append(task)
-                if pending_lists:
-                    pending_tasks[employee_name]=pending_lists
-                    return pending_tasks
-add_employee("Ahmad")
+    for employee in Employees:
+        for task in Employees[employee]:
+            if task["Status"]=="Pending":
+                pending_tasks.append({"employee":employee,"task_name":task["task_name"]})
+                return pending_tasks
+
+add_employee("Alla")
+add_employee("Ayesha")
 add_employee("Laiba")
-add_employee("Ashar")
-assign_task("Ahmad","Designing")
-assign_task("Ashar","Fix Bugs")
-assign_task("Laiba","Frontend")
-complete_task("Laiba","Frontend")
-print("\n All Employees task")
-print("\n Ahamd Task")
-print(get_employee_tasks("Ahmad"))
-print("\n Pending Tasks")
+assign_task("Alla","Create a presentation")
+assign_task("Ayesha","Create a SRS document")
+assign_task("Laiba","Create a Report")
+complete_task("Laiba","Create a Report")
+print(get_employee_tasks("Alla"))
+print(get_employee_tasks("Ayesha"))
+print(get_employee_tasks("Laiba"))
 print(get_pending_tasks())
+
