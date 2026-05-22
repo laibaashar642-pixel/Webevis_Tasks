1b069c9481730d35072505d2f8ac745c16ed0239
Laiba Ashar <asharmunir642@gmail.com>
2026-05-22 12:39:18 +0500

    Advanced Version

--- Employee Task Tracker System/advance_tracker.py
+++ Employee Task Tracker System/advance_tracker.py
@@ -1,37 +1,74 @@
 #Main Logic  firstly we need to understand whether manager exists or not check employee exists or not check duplicate task check task record  add into employee taask so actual implementation made data stored in the form of dictionary tasks move through  functions or if else structure functions make checking by using if else structure
 Employees={
    "Umair":{"Role":"Manager",
-            "Task":"Designing"},
+            "Tasks":[]},
    "Laiba":{"Role":"Employee",
-            "Task":"Development"},
+            "Tasks":[]},
    "Ashar":{"Role":"Employee",
-            "Task":"Testing"},
+            "Tasks":[]},
    "Zainab":{"Role":"Employee",
-            "Task":"Documentation"},
+            "Tasks":[]},
    "Sana":{"Role":"Employee",
-            "Task":"Maintenance"},
+            "Tasks":[]},
    "Zain":{"Role":"Employee",
-            "Task":"Support"},
-   "Mahnoor":{"Role":"HR"},
-   "Kajal":{"Role":"Employee"},
-   "Sadeeq":{"Role":"Supervisor"},
+            "Tasks":[]},
+   "Mahnoor":{"Role":"HR",
+              "Tasks":[]},
+   "Kajal":{"Role":"Employee",
+            "Tasks":[]},
+   "Sadeeq":{"Role":"Supervisor",
+             "Tasks":[]},
 }
-name=input("Enter Employee Name:")
-if name in Employees:
-    print("Employee Exists")
 
-    role_check = Employees[name]["Role"] == "Manager"
-
-    if role_check:
-        print("Manager Exists")
-    else:
-        print("Not a Manager")
-
-else:
-    print("Employee Not Exists")
 #Actual Implementation is Assigning Task System Where the logic starts here Assigning Task System
-task_name=input("Enter Task Name:")
-if  Employees[name]["Task"]==task_name:
-    print("Task Exists")
-else:
-    print("Task Not Exists")
+
+Tasks=["Designing","Testing","Documentation","Deployment"]
+
+#Adding Functions
+def add_employee(name,role):
+    if name in Employees:
+       print("Employee Already Exists")
+    
+    else:
+        Employees[name]={"Role": role, "Tasks": []}
+        print(f"Employee '{name}' added successfully")
+def assign_task(manager_name,employee_name,task_name):
+    if manager_name not in Employees:
+     print("Manager Not Exists")
+     return
+    if Employees[manager_name]["Role"] != "Manager":
+     print("Only Manager Can Assign Tasks")
+     return
+    if employee_name not in Employees:
+        print("Employee Not Exists")
+        return
+    if manager_name==employee_name:
+       print("Manager Cannot Assign Task To Themselves")
+       return
+   
+    if task_name not in Tasks:
+       print("Invalid Task")
+       return
+    
+    for task in Employees[employee_name]["Tasks"]:
+     if task["task_name"] == task_name:
+            print("Task already assigned")
+            return
+
+   
+
+    Employees[employee_name]["Tasks"].append(
+       {"task_name": task_name, "Status": "Pending"})
+    print(f"Task '{task_name}' assigned to {employee_name}")
+
+
+# Testing the implementation
+add_employee("Ali", "Employee")
+assign_task("Umair", "Ali", "Designing")
+
+print(Employees["Ali"]["Tasks"])
+
+
+
+
+
